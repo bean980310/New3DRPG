187a02fdbd28c4563f3f6a71fd6521e1cc1727dc
bean980310 <bean980310@hotmail.co.kr>
2016-08-12 13:27:01 +0900

    Character Selection

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,36 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
     public List<Character> Characters = new List<Character>();
+    bool ShowCharWheel;
+    public int SelectedCharacter;
 	// Use this for initialization
 	void Start () {
-	
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ShowCharWheel = true;
+        }
 	}
+
+    void OnGUI()
+    {
+        if (ShowCharWheel)
+        {
+            GUILayout.BeginArea(new Rect(Screen.width/2, Screen.height/2, 512, 512), "box");
+            foreach(Character c in Characters)
+            {
+                if (GUILayout.Button(c.Name))
+                {
+                    SelectedCharacter = Characters.IndexOf(c);
+                }
+            }
+            GUILayout.EndArea();
+        }
+    }
 }
 [System.Serializable]
 public class Character
